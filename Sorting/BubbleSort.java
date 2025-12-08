684c8b8eba0e4e64fb4666363e2301062b33040d
Samir Zade <samirzade@Samirs-MacBook-Air.local>
2025-12-08 20:32:32 +0530

    sorting started to learn

---
+++ Sorting/BubbleSort.java
@@ -0,0 +1,34 @@
+package Sorting;
+
+public class BubbleSort {
+
+    //write the function
+    public static void makeArrSort(int arr[]) {
+
+        for (int turn = 0; turn < arr.length - 1; turn++) {
+            for (int j = 0; j < arr.length - 1 - turn; j++) {
+                //swap the values
+                if (arr[j] > arr[j+1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+    }
+
+    //write function to print values
+    public static void printArr(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    //write the main static
+    public static void main(String[] args) {
+        int arr[] = {5, 4, 2, 3, 1};
+        makeArrSort(arr);
+        printArr(arr);
+    }
+}
